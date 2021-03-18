6abc501aeb4be41513a47c5d8607d5149f1e878c
HubertCuris <hubert.curis@yahoo.fr>
2021-03-18 15:19:24 +0100

    products list updated

--- scripts/product_lists.js
+++ scripts/product_lists.js
@@ -52,52 +52,52 @@ const laptopProducts = [{
 
 const smartphoneProducts = [{
   id: 201,
-  product_name: 'MSI - GF65 Thin 9SEXR-694XFR - Noir',
+  product_name: 'Huawei - P1 - Bleu',
   price: 899.99,
   pic_url: ['201-1.jpg', '201-2.jpg'] 
 }, {
   id: 202,
-  product_name: 'ASUS - ZenBook 14 - UX434FA-A9203T',
+  product_name: 'Honor - X23 - navy',
   price: 779.99,
   pic_url: ['202-1.jpg', '202-2.jpg'] 
 }, {
   id: 203,
-  product_name: 'MSI - GE66 Raider 20SF-407FR',
+  product_name: 'Samsung - Galaxy 40',
   price: 1549.99,
   pic_url: ['203-1.png', '203-2.png'] 
 }, {
   id: 204,
-  product_name: 'ASUS - ROG Strix G17-G712LV-H7008T',
+  product_name: 'Samsung - Galaxy P20',
   price: 1399.98,
   pic_url: ['204-1.png', '204-2.png'] 
 }, {
   id: 205,
-  product_name: 'HUAWEI - MateBook D 14 2020 - 53010TVR',
+  product_name: 'HUAWEI - D 14 2020 - noir',
   price: 759.99,
   pic_url: ['205-1.png', '205-2.png'] 
 }, {
   id: 206,
-  product_name: 'GIGABYTE - AORUS 15P KB-7FR1130SH',
+  product_name: 'HUAWEI - Z23 - noir',
   price: 1599.99,
   pic_url: ['206-1.png', '206-2.png'] 
 }, {
   id: 207,
-  product_name: 'APPLE - MacBook Air 13 - 128 Go - MQD32FN/A',
+  product_name: 'Xiaomi - H2 - 128 Go - noir',
   price: 899.99,
   pic_url: ['207-1.jpg', '207-2.jpg'] 
 }, {
   id: 208,
-  product_name: 'MSI - Modern 14 B4MW-033XFR',
+  product_name: 'Xiaomi - H3a - 56 Go',
   price: 749.99,
   pic_url: ['208-1.jpg', '208-2.jpg'] 
 }, {
   id: 209,
-  product_name: 'MSI - Alpha 15 A3DDK-011FR',
+  product_name: 'Iphone 20 - bleu',
   price: 949.99,
   pic_url: ['209-1.jpg', '209-2.jpg'] 
 }, {
   id: 210,
-  product_name: 'ASUS - ROG Zephyrus S GX535GW-ES040R',
+  product_name: 'Iphone 10xy - rose',
   price: 1699.98,
   pic_url: ['210-1.png', '210-2.png'] 
 }]
@@ -105,52 +105,52 @@ const smartphoneProducts = [{
 
 const mowerProducts = [{
   id: 301,
-  product_name: 'MSI - GF65 Thin 9SEXR-694XFR - Noir',
+  product_name: 'Ryobi -  EDJ45',
   price: 899.99,
   pic_url: ['301-1.jpg', '301-2.jpg'] 
 }, {
   id: 302,
-  product_name: 'ASUS - ZenBook 14 - UX434FA-A9203T',
+  product_name: 'Xtrem - TriCycle - UFA-A903X',
   price: 779.99,
   pic_url: ['302-1.jpg', '302-2.jpg'] 
 }, {
   id: 303,
-  product_name: 'MSI - GE66 Raider 20SF-407FR',
+  product_name: 'Bosch - A40THLM3',
   price: 1549.99,
   pic_url: ['303-1.jpg', '303-2.jpg'] 
 }, {
   id: 304,
-  product_name: 'ASUS - ROG Strix G17-G712LV-H7008T',
+  product_name: 'John Deere - GJK34',
   price: 1399.98,
   pic_url: ['304-1.png', '304-2.jpg'] 
 }, {
   id: 305,
-  product_name: 'HUAWEI - MateBook D 14 2020 - 53010TVR',
+  product_name: 'Philipps - STI43',
   price: 759.99,
   pic_url: ['305-1.jpg', '305-2.jpg'] 
 }, {
   id: 306,
-  product_name: 'GIGABYTE - AORUS 15P KB-7FR1130SH',
+  product_name: 'Bosch - 34JKH5',
   price: 1599.99,
   pic_url: ['306-1.jpg', '306-2.jpg'] 
 }, {
   id: 307,
-  product_name: 'APPLE - MacBook Air 13 - 128 Go - MQD32FN/A',
+  product_name: 'Xiobi - 23JF-452T',
   price: 899.99,
   pic_url: ['307-1.jpg', '307-2.jpg'] 
 }, {
   id: 308,
-  product_name: 'MSI - Modern 14 B4MW-033XFR',
+  product_name: 'MCjo - stu305ff67',
   price: 749.99,
   pic_url: ['308-1.jpg', '308-2.jpg'] 
 }, {
   id: 309,
-  product_name: 'MSI - Alpha 15 A3DDK-011FR',
+  product_name: 'ModernFuture - Motudj442S',
   price: 949.99,
   pic_url: ['309-1.jpg', '309-2.jpg'] 
 }, {
   id: 310,
-  product_name: 'ASUS - ROG Zephyrus S GX535GW-ES040R',
+  product_name: 'BlOCC - GKYL-GDKZ34',
   price: 1699.98,
   pic_url: ['310-1.png', '310-2.png'] 
 }]
@@ -159,52 +159,52 @@ const mowerProducts = [{
 
 const cameraProducts = [{
   id: 401,
-  product_name: 'MSI - GF65 Thin 9SEXR-694XFR - Noir',
+  product_name: 'Lumix GC34 - Noir',
   price: 899.99,
   pic_url: ['401-1.jpg', '401-2.jpg'] 
 }, {
   id: 402,
-  product_name: 'ASUS - ZenBook 14 - UX434FA-A9203T',
+  product_name: 'Sony GT56 Noir',
   price: 779.99,
   pic_url: ['402-1.png', '402-2.png'] 
 }, {
   id: 403,
-  product_name: 'MSI - GE66 Raider 20SF-407FR',
+  product_name: 'Sony - HDR34',
   price: 1549.99,
   pic_url: ['403-1.png', '403-2.jpg'] 
 }, {
   id: 404,
-  product_name: 'ASUS - ROG Strix G17-G712LV-H7008T',
+  product_name: 'Lumix - GKLT56',
   price: 1399.98,
   pic_url: ['404-1.jpg', '404-2.jpg'] 
 }, {
   id: 405,
-  product_name: 'HUAWEI - MateBook D 14 2020 - 53010TVR',
+  product_name: 'Nikon RX34 ',
   price: 759.99,
   pic_url: ['405-1.jpg', '405-2.jpg'] 
 }, {
   id: 406,
-  product_name: 'GIGABYTE - AORUS 15P KB-7FR1130SH',
+  product_name: 'Nikon TR54HD',
   price: 1599.99,
   pic_url: ['406-1.jpg', '406-2.png'] 
 }, {
   id: 407,
-  product_name: 'APPLE - MacBook Air 13 - 128 Go - MQD32FN/A',
+  product_name: 'Sony HGJT2',
   price: 899.99,
   pic_url: ['407-1.jpg', '407-2.jpg'] 
 }, {
   id: 408,
-  product_name: 'MSI - Modern 14 B4MW-033XFR',
+  product_name: 'Lumix HDRt34',
   price: 749.99,
   pic_url: ['408-1.jpg', '408-2.jpg'] 
 }, {
   id: 409,
-  product_name: 'MSI - Alpha 15 A3DDK-011FR',
+  product_name: 'Lumix 3049',
   price: 949.99,
   pic_url: ['409-1.jpg', '409-2.jpg'] 
 }, {
   id: 410,
-  product_name: 'ASUS - ROG Zephyrus S GX535GW-ES040R',
+  product_name: 'Lumix md34R',
   price: 1699.98,
   pic_url: ['410-1.jpg', '410-2.jpg'] 
 }]
